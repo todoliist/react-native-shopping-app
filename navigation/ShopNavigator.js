3a2deb060c6b1b9ac5e0be133adddf4ed795c62b
Tim Su <tsu@fieldflex.com>
2020-10-13 18:48:58 -0600

    create products overview and detail page

--- navigation/ShopNavigator.js
+++ navigation/ShopNavigator.js
@@ -2,14 +2,22 @@ import { createAppContainer } from 'react-navigation'
 import { createStackNavigator } from 'react-navigation-stack'
 import { Platform } from 'react-native'
 import ProductsOverviewScreen from '../screens/shop/ProductsOverviewScreen'
+import ProductDetailScreen from '../screens/shop/ProductDetailScreen'
 import Colors from '../constants/Colors'
 
 const ProductsNavigator = createStackNavigator({
-    ProductsOverview: ProductsOverviewScreen
+    ProductsOverview: ProductsOverviewScreen,
+    ProductDetail: ProductDetailScreen
 }, {
     defaultNavigationOptions: {
         headerStyle: {
             backgroundColor: Platform.OS === 'andriod' ? Colors.primary : ''
+        },
+        headerTitleStyle:{
+            fontFamily: 'open-sans-bold'
+        },
+        headerBackTitleStyle:{
+            fontFamily: 'open-sans'
         },
         headerTintColor: Platform.OS === 'andriod' ? 'white' : Colors.primary
     }
